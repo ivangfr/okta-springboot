85148ce6342aa8473f67e179715fc3cb7162782a
Ivan Franchin <ivan.franchin@takeaway.com>
2023-05-28 08:49:19 -0300

    Project upgrade - upgrade to spring-boot 3.1.0; - upgrade to springdoc-openapi 2.1.0; - upgrade to jib 3.3.2; - refactor securityFilterChain bean as the previous implementation was using deprecated methods.

--- simple-service/src/main/java/com/ivanfranchin/simpleservice/security/WebSecurityConfig.java
+++ simple-service/src/main/java/com/ivanfranchin/simpleservice/security/WebSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,16 +16,18 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET, "/private").authenticated()
-                .requestMatchers(HttpMethod.GET, "/public").permitAll()
-                .requestMatchers(HttpMethod.POST, "/callback/token").permitAll()
-                .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-                .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
-                .anyRequest().authenticated();
-        http.oauth2ResourceServer().jwt();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.cors().and().csrf().disable();
-        return http.build();
+        return http
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers(HttpMethod.GET, "/private").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/public").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/callback/token").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
     }
 }
